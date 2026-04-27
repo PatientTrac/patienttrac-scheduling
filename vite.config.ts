00cdd05c1cd2142cf892ae4091b16416188b0400
PatientTrac <wayne@wayluz.com>
2026-04-27 10:03:11 -0500

    fix: vite alias for packages/ui inside repo

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@patienttrac/ui': path.resolve(__dirname, '../packages/ui/src'),
+      '@patienttrac/ui': path.resolve(__dirname, './packages/ui/src'),
     },
   },
   build: {
